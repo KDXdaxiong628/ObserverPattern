e6fe493d592fafc7ccd50af2b2af10cc54f74438
KDXdaxiong628 <728772017@qq.com>
2017-09-28 15:48:28 +0800

    Initial Commit

---
+++ ObserverPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObserverPattern
+//
+//  Created by 大雄 on 2017/9/28.
+//  Copyright © 2017年 CoderDX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObserverPattern/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObserverPattern
+//
+//  Created by 大雄 on 2017/9/28.
+//  Copyright © 2017年 CoderDX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
